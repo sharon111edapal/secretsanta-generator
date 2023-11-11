55f9aadfff2d8e876f46f2e653395130d3075081
IamGroot <107915199+sharon111edapal@users.noreply.github.com>
2023-11-11 23:05:53 +0400

    eee

---
+++ script/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull sharon111edapal/projectsanta:v1
+
+# Run the Docker image as a container
+docker run -d -p 8888:8080 sharon111edapal/projectsanta:v1
