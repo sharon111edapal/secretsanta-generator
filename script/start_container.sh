93676cbab8d96699886da25d5aafb2f6b079b912
IamGroot <107915199+sharon111edapal@users.noreply.github.com>
2023-11-13 12:31:16 +0400

    Update start_container.sh

--- script/start_container.sh
+++ script/start_container.sh
@@ -4,5 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull sharon111edapal/projectsanta:v1
 
-# Run the Docker image as a container
+# Run the Docker image as a container ##
 docker run -d -p 8888:8080 sharon111edapal/projectsanta:v1
